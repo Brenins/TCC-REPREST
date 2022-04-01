4dc986711197f98b73f9ba6fd4788df1ca4dbe08
Breno Ramos <70190197+Brenins@users.noreply.github.com>
2022-03-31 22:44:38 -0300

    Edições adicionadas

--- admin/cadastros/categorias.php
+++ admin/cadastros/categorias.php
@@ -1,7 +1,18 @@
 <?php 
     //se n existir variavel page
     if(!isset($page)) exit;
-    
+    $nome = NULL;
+    if(!empty($id)){
+        $sql = "select * from categoria where id = :id limit 1";
+        $consulta = $pdo->prepare($sql);
+        $consulta->bindParam(":id",$id);
+        $consulta->execute();
+
+        $dados = $consulta->fetch(PDO::FETCH_OBJ);
+
+        $id = $dados->id ?? NULL;
+        $nome = $dados->nome ?? NULL;
+    }
 ?>
 
 <div class="card">
@@ -14,16 +25,18 @@
         </div>
     </div>
     <div class="card-body">
-        <form name="formCadastro" method="post" action="salvar/categorias">
+        <form name="formCadastro" method="post" action="salvar/categorias" data-parsley-validate="">
             <label for="id">Id da Categoria:</label>
             <input type="text" readonly name="id" id="id"
-            class="form-control">
+            class="form-control" value="<?=$id?>">
             <label for="nome">Nome da Categoria:</label>
-            <input type="text" name="caegoria" id="nome"
-            class="form-control" required data-parsley-required-message="Por favor, preencha este campo">
+            <input type="text" name="nome" id="nome"
+            class="form-control"  required 
+            data-parsley-required-message="Por favor, preencha este campo"
+            value="<?=@$nome?>">
             <br>
             <button type="submit" class="btn btn-success">
-                <i class="fas fa-check"></i>Salvar Dados
+                <i class="fas fa-check"></i>
             </button>
         </form>
     </div>
