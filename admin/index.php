8c72e1560df1070b1494c46d47eca3607b208aae
Breno Ramos <breninsramos@gmail.com>
2022-11-15 15:07:41 -0300

    sdfb

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Sistema de e-commerce BurnsShop">
+    <meta name="description" content="REPREST">
     <meta name="author" content="">
 
     <title>REPREST</title>
